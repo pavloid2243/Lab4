7cea40fc5b9439d948f36164b4fd41d59a637c59
Fedor T <56504969+pavloid2243@users.noreply.github.com>
2021-11-01 15:18:06 +0800

    Add files via upload

---
+++ Laba4/Laba4/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laba4
+{
+    public partial class Form1 : Form
+    {
+        public static Random rnd = new Random();
+        List<Plants> plantsList = new List<Plants>();
+        public Form1()
+        {
+            InitializeComponent();
+            ShowInfo();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.plantsList.Clear();
+            
+            for (var i = 0; i < 10; ++i)
+            {
+                switch(rnd.Next()%3)
+                {
+                    case 0:
+                        this.plantsList.Add(Flower.Generate());
+                        break;
+                    case 1:
+                        this.plantsList.Add(Shrub.Generate());
+                        break;
+                    case 2:
+                        this.plantsList.Add(Tree.Generate());
+                        break;
+                }
+            }
+            ShowInfo();
+            Queue();
+        }
+        private void Queue()
+        {
+            
+            String str = "Очередь:\n";
+            //richTextBox3.Text = String.Format("Очередь\n1.{0}\n2.{1}\n3.{2}\n4.{3}\n5.{4}\n6.{5}\n7.{6}\n8.{7}\n9.{8}\n10.{9}",
+                for (int i=0;i<this.plantsList.Count;i++)
+            {
+                var plant = this.plantsList[i];
+                if (plant is Flower) str += $"{i+1} " + "Цветок\n";
+                if (plant is Shrub) str += $"{i+1} " + "Кустарник\n";
+                if (plant is Tree) str += $"{i+1} " + "Дерево\n";
+            }
+            richTextBox3.Text = str;
+
+        }
+        private void ShowInfo()
+        {
+            int floawerCount = 0;
+            int shrubCount = 0;
+            int treesCount = 0;
+
+            foreach( var a in this.plantsList)
+            {
+                if (a is Flower) {
+                    floawerCount++;
+                }
+                if (a is Shrub) {
+                    shrubCount++;
+                }
+                if (a is Tree) { 
+                    treesCount++;
+                }
+            }
+            richTextBox1.Text = "Цветы\tКустарники\tДеревья";
+            richTextBox1.Text += "\n";
+            richTextBox1.Text += String.Format("{0}\t{1}\t\t{2}", floawerCount, shrubCount,treesCount);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (this.plantsList.Count == 0)
+            {
+                richTextBox2.Text = "";
+                return;
+            }
+            var plant = this.plantsList[0];
+            this.plantsList.RemoveAt(0);
+            richTextBox2.Text = plant.GetInfo();
+            ShowInfo();
+            Queue();
+        }
+    }
+}
---
+++ Laba4/Laba4/Plants.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Laba4
+{
+    public class Plants
+    {
+        public static Random rnd = new Random();
+        public int height = 0;
+        public virtual String GetInfo()
+        {
+            var str = String.Format("\nВысота: {0}", this.height);
+            return str;
+        }
+
+    }
+    public enum Color { белый,красный,желтый,синий };
+    public enum Type {Роза, Хризантема,Тюльпан };
+    public class Flower : Plants
+    {
+        public int number = 0;
+        public Color color =  Color.синий;
+        public Type type = Type.Роза ;
+        public override String GetInfo()
+        {
+            var str = "Цветок";
+            str += String.Format("\nКоличество липестков: {0}\nЦвет: {1}\nТип: {2}", this.number,this.color,this.type);
+            str += base.GetInfo();
+            return str;
+        }
+
+        public static Flower Generate()
+        {
+
+            return new Flower
+            {
+                number = rnd.Next() % 21,
+                color = (Color)rnd.Next(4),
+                type = (Type)rnd.Next(3),
+                height = rnd.Next() % 5 +1
+
+            };
+        }
+    }
+    public class Shrub : Plants
+    {
+        public bool isFlower = false;
+        public int number = 0;
+        public override String GetInfo()
+        {
+            var str = "Кустарник";
+            str += String.Format("\nНаличие цветов: {0}\nКоличество веточек: {1}", this.isFlower,this.number);
+            str += base.GetInfo();
+            return str;
+
+        }
+        public static Shrub Generate()
+        {
+
+            return new Shrub
+            {
+                number = rnd.Next() % 21,
+                isFlower =rnd.Next()%2==0,
+                height = rnd.Next() % 10 +1
+
+
+            };
+        }
+    }
+    public enum TypeTree { Хвойное, Листовое };
+    public class Tree : Plants
+    {
+       
+        public TypeTree typetree = TypeTree.Листовое;
+        public int Rad = 0;
+        public override String GetInfo()
+        {
+            var str = "Дерево";
+            str += base.GetInfo();
+            str += String.Format("\nТип: {0}\nРадиус: {1}", this.typetree,this.Rad);
+            return str;
+        }
+        public static Tree Generate()
+        {
+
+            return new Tree
+            {
+                height = rnd.Next() % 100 + 1,
+                typetree =(TypeTree)rnd.Next(2),
+                Rad=rnd.Next()%100 + 20
+
+
+            };
+        }
+
+    }
+}
